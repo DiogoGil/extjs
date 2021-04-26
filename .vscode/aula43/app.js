d2db03dbeb379363208d980dd0b13cce73beaafe
Diogo <Diogo.gil@icompsoft.com.br>
2021-04-26 17:02:16 -0300

    last change

--- .vscode/aula43/app.js
+++ .vscode/aula43/app.js
@@ -1,34 +1,38 @@
-Ext.create('Ext.data.Store',{
-    storeId: 'simpsonsStore',
-    fields: ['name', 'email', 'phone'],
-    data: {'itens': [
-        {'name': 'Lisa', 'email': 'lisa@simpsons.com', 'phone':'555-222-111'},
-        {'name': 'Bart', 'email': 'bart@simpsons.com', 'phone':'555-222-111'},
-        {'name': 'Homer', 'email': 'homer@simpsons.com', 'phone':'555-222-111'},
-        {'name': 'Marge', 'email': 'marge@simpsons.com', 'phone':'555-222-111'}
-    ]},
-    proxy: {type: 'memory',
-    reader:{
-        type: 'json',
-        root: 'items'
-    }}
-})
-
-Ext.define('Curso.Grid',{
-    extend: 'Ext.panel.Grid',
-
-    title: 'Aula 43',
-    store: Ext.data.StoreManager.lookup('simpsonsStore'),
-    columns: [
-        {text: 'Name', dataIndex: 'name'},
-        {text: 'Email', dataIndex: 'email',flex: 1},
-        {text: 'Phone', dataIndex: 'phone'}
-    ],
-    height: 200,
-    width: 400,
-    renderTo: Ext.getBody(),
-});
-
-Ext.onReady(function(){
-    Ext.create('Curso.Janela');
+Ext.onReady(function() {
+    Ext.create('Ext.container.Viewport',{
+        layout: 'border',
+        items: [{
+            region: 'north',
+            html: <h1 class="x-panel-header">Page Title</h1>,
+            border: false,
+            margins: '0 0 5 0'
+        },{
+            region: 'west',
+            collapsible: true,
+            title: 'Navigation',
+            width: 150
+        },{
+            region: 'south',
+            title: 'South Panel',
+            collapsible: true,
+            html: 'Information goes here',
+            split: true,
+            height: 100,
+            minHeight: 100,
+        },{
+            region: 'east',
+            title: 'East Panel',
+            collapsible: true,
+            split: true,
+            width: 150,
+        },{
+            region: 'center',
+            xtype: 'tabpanel',
+            activeTab: 0,
+            items: {
+                title: 'Default Table',
+                html :'The first tab\'s content. Others may be added dynamically.',
+            }
+        }]
+    })
 })
